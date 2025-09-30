90eebcb3bfa13a44ae1f7bb6e936388b064f3147
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-09-29 21:32:29 -0500

    Arrays.

--- StructureProgramming/Program.cs
+++ StructureProgramming/Program.cs
@@ -4,3 +4,18 @@ Console.WriteLine("Hello, World!");
 // Infiere el tipo de dato
 var num = 15;
 var name = "Juan";
+
+// === Arrays ===
+
+int[] numeros = new int[5];
+numeros[0] = 1;
+numeros[1] = 2;
+numeros[2] = 3;
+numeros[3] = 4;
+numeros[4] = 5;
+
+var numbers = new int[5]{
+    1,2,3,4,5
+};
+
+Console.WriteLine(numbers[2]);
