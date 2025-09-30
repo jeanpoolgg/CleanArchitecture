2ef1ad719861027f82b5e8ca50bbcf26e068fd01
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-09-29 21:57:27 -0500

    Sentencias condicionales.

--- StructureProgramming/Program.cs
+++ StructureProgramming/Program.cs
@@ -19,3 +19,33 @@ var numbers = new int[5]{
 };
 
 Console.WriteLine(numbers[2]);
+
+
+// === Sentencias condicionales ===
+var age = 12;
+
+if(age > 60)
+{
+    Console.WriteLine("Es de la tercera edad");
+}
+else if(age > 18)
+{
+    Console.WriteLine("Es mayor de edad");
+}
+else
+{
+    Console.WriteLine("Es menor de edad");
+}
+
+switch(age)
+{
+    case < 18:
+        Console.WriteLine("Es menor de edad");
+        break;
+    case < 60:
+        Console.WriteLine("Es mayor de edad");
+        break;
+    default:
+        Console.WriteLine("Es de la tercera edad");
+        break;
+}
