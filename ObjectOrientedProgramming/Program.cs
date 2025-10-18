d6d85b0e03057393f429c2a7f6d423e8fcbf0a6f
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-10-18 18:38:10 -0500

    Clases y Objetos

---
+++ ObjectOrientedProgramming/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Beer erdingerBeer = new Beer()
+{
+    Name = "Erdinger",
+    Price = 3
+};
+
+var coronaBeer = new Beer();
+coronaBeer.Name = "Corona";
+coronaBeer.Price = 1.5m;
+
+Console.WriteLine(erdingerBeer.Name);
+Console.WriteLine(erdingerBeer.Name + " $ " + erdingerBeer.Price);
+Console.WriteLine(erdingerBeer.GetInfo());
+
+
+public class Beer
+{
+    public string Name { get; set; }
+    public decimal Price { get; set;}
+
+    public string GetInfo()
+    {
+        return "Nombre: " + Name + ", Precio: $ " + Price;
+    }
+}
