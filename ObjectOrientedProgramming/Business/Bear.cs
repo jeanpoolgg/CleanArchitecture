f0355c40561371c950c71a1cfb803fc35a7e77a5
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-10-18 21:21:08 -0500

    Espacio de nombres (Namespace)

---
+++ ObjectOrientedProgramming/Business/Bear.cs
@@ -0,0 +1,47 @@
+﻿namespace ObjectOrientedProgramming.Business
+{
+    public class Beer
+    {
+        // Campos
+        private decimal _alcohol;
+
+        // Propiedades
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public decimal Alcohol
+        {
+            get { return _alcohol; }
+            set
+            {
+                if (value < 0)
+                {
+                    value = 0;
+                }
+                _alcohol = value;
+            }
+        }
+
+        public string SAlcohol
+        {
+            get
+            {
+
+                return "Alcohol: " + _alcohol.ToString();
+            }
+
+        }
+
+        public Beer(string name, decimal price, decimal alcohol)
+        {
+            Name = name;
+            Price = price;
+            Alcohol = alcohol;
+        }
+
+        public string GetInfo()
+        {
+            return "Nombre: " + Name + ", Precio: $ " + Price + ", Alcohol: " + Alcohol;
+        }
+    }
+}
--- ObjectOrientedProgramming/Program.cs
+++ ObjectOrientedProgramming/Program.cs
@@ -1,50 +1,8 @@
 ﻿
+using ObjectOrientedProgramming.Business;
+
 Beer erdingerBeer = new Beer("Erdinger", 3, -2);
 
 Console.WriteLine(erdingerBeer.GetInfo());
 Console.WriteLine(erdingerBeer.SAlcohol);
 
-public class Beer
-{
-    // Campos
-    private decimal _alcohol;
-
-    // Propiedades
-    public string Name { get; set; }
-    public decimal Price { get; set; }
-    
-    public decimal Alcohol
-    {
-        get { return _alcohol; }
-        set
-        {
-            if (value < 0)
-            {
-                value = 0;
-            }
-            _alcohol = value;
-        }
-    }
-
-    public string SAlcohol
-    {
-        get
-        {
-
-            return "Alcohol: " + _alcohol.ToString();
-        }
-        
-    }
-
-    public Beer(string name, decimal price, decimal alcohol)
-    {
-        Name = name;
-        Price = price;
-        Alcohol = alcohol;
-    }
-
-    public string GetInfo()
-    {
-        return "Nombre: " + Name + ", Precio: $ " + Price + ", Alcohol: " + Alcohol;
-    }
-}
