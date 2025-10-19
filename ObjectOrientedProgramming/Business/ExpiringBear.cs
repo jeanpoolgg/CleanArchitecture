7d684f26157397d715ad19849365971e5aa4942e
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-10-19 10:21:14 -0500

    Herencia

---
+++ ObjectOrientedProgramming/Business/ExpiringBear.cs
@@ -0,0 +1,13 @@
+﻿
+namespace ObjectOrientedProgramming.Business
+{
+    public class ExpiringBear : Beer
+    {
+        public DateTime ExpirationDate { get; set; }
+
+        public ExpiringBear(string name, decimal price, decimal alcohol, DateTime expirationDate) : base(name, price, alcohol)
+        {
+            ExpirationDate = expirationDate;
+        }
+    }
+}
--- ObjectOrientedProgramming/Program.cs
+++ ObjectOrientedProgramming/Program.cs
@@ -2,7 +2,7 @@
 using ObjectOrientedProgramming.Business;
 
 Beer erdingerBeer = new Beer("Erdinger", 3, -2);
+var delirium = new ExpiringBear("Delirium Tremens", 4.5m, 8.5m, new DateTime(2025, 12, 01));
 
 Console.WriteLine(erdingerBeer.GetInfo());
-Console.WriteLine(erdingerBeer.SAlcohol);
-
+Console.WriteLine(delirium.GetInfo());
