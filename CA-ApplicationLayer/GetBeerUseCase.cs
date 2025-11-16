a324a7c7d89ac963d6129b85d21e86dd9fd0cf5e
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-11-16 11:01:10 -0500

    Use Cases (Capa Application Business Rules)

---
+++ CA-ApplicationLayer/GetBeerUseCase.cs
@@ -0,0 +1,21 @@
+﻿using CA_EnterpriseLayer;
+
+namespace CA_ApplicationLayer
+{
+   public class GetBeerUseCase
+    {
+        private readonly IRepository _beerRepository;
+
+        public GetBeerUseCase(IRepository beerRepository )
+        {
+            _beerRepository = beerRepository;
+        }
+
+        public async Task<IEnumerable<Beer>> ExecuteAsync()
+        {
+            return await _beerRepository.GetAllAsync();
+        }
+
+    }
+
+}
---
+++ CA-ApplicationLayer/IRepository.cs
@@ -0,0 +1,13 @@
+﻿
+
+using CA_EnterpriseLayer;
+
+namespace CA_ApplicationLayer
+{
+    public interface IRepository
+    {
+        Task<Beer> GetByIdAsync(int id);
+        Task<IEnumerable<Beer>> GetAllAsync();
+        Task AddAsync(Beer beer);
+    }
+}
