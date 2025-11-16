04445d6961c5e3fc92f97ed6bb1779a572e317be
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-11-16 10:23:53 -0500

    Entities (Capa Enterprise Business Rules)

---
+++ CA-EnterpriseLayer/Beer.cs
@@ -0,0 +1,11 @@
+﻿namespace CA_EnterpriseLayer
+{
+    public class Beer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Style { get; set; }
+        public decimal Alcohol { get; set; }
+        public bool IsStrongBeer() => Alcohol > 7.5m;
+    }
+}
