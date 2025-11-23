3838691415557a35ddec4ed443f553cd8249cc5e
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-11-22 19:48:39 -0500

    Repository (Capa Interface Adapters)

---
+++ CA-InterfacesAdapters-Repository/Repository.cs
@@ -0,0 +1,33 @@
+﻿using CA_ApplicationLayer;
+using CA_EnterpriseLayer;
+using CA_InterfaceAdapters_Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CA_InterfacesAdapters_Repository
+{
+    public class Repository : IRepository
+    { 
+        private readonly AppDbContext _dbContext;
+
+        public Repository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddAsync(Beer beer)
+        {
+            await _dbContext.Beers.AddAsync(beer);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Beer>> GetAllAsync()
+        {
+            return await _dbContext.Beers.ToListAsync();
+        }
+
+        public async Task<Beer> GetByIdAsync(int id)
+        {
+            return await _dbContext.Beers.FindAsync(id);
+        }
+    }
+}
